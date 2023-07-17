e86ad412648aa4157cf8ca38e13d515ee55755d9
MunyinyaMaurice <munyinya13@gmail.com>
2023-07-18 01:11:34 +0200

    variable declaration-and-initialization

---
+++ task83/solution.js
@@ -0,0 +1,4 @@
+let Welcome_message = "Hello!";
+let CodingSchedule = "24/7";
+let IS_WATCHER_ENABLED = true;
+let Salary = 300000;
---
+++ task87/solution.js
@@ -0,0 +1,12 @@
+/**
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+
+const name1='Shema1'
+const name2='Shema2'
+const name3='Shema3'
+const name4='Shema4'
+const name5='Shema5'
+console.log(name3)
