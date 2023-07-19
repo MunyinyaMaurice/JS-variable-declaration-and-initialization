c3f372969b41c13dac06fb1d9969909b466b3f75
MunyinyaMaurice <munyinya13@gmail.com>
2023-07-19 23:53:39 +0200

    export and import const

---
+++ task97/constants.js
@@ -0,0 +1,12 @@
+export const users = {
+  1: {
+    name: 'Connor'+' '+'shema',
+  },
+  2: {
+    name: 'Harry',
+  },
+  3: {
+    name: 'Jack',
+  },
+};
+
---
+++ task99/solution.js
@@ -0,0 +1,5 @@
+import { rideTime,rideSpeed } from "./constants.js";
+let rs= rideSpeed;
+let rt= rideTime;
+let df= rs -rt;
+console.log(df);
