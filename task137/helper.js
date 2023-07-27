d3972284e4a9690b1ad2407bcb8aa042a660f56f
MunyinyaMaurice <munyinya13@gmail.com>
2023-07-28 00:42:40 +0200

    Positive thinking

--- task137/helper.js
+++ task137/helper.js
@@ -1,3 +1,9 @@
 export const positiveElements = (arr) => {
-  return arr;
+  let temp=[];
+  for(let i=0; i<arr.length;i++){
+    if(arr[i]>=0){
+      temp.push (arr[i]);
+    }
+  }
+  return temp;
 }
